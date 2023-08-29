356813586df1ce0252d8ee10189ee94867be1e78
Dmitrii Boykov <boikovdmitriy@mail.ru>
2023-08-29 10:51:43 +0400

    added storybook to the app

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,15 @@
+async function loadTailwindPlugin() {
+  try {
+    const plugin = await import('prettier-plugin-tailwindcss')
+    return plugin.default
+  } catch (error) {
+    console.error('Failed to load prettier-plugin-tailwindcss:', error)
+    return null
+  }
+}
 module.exports = {
   semi: false,
   singleQuote: true,
   trailingComma: 'all',
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: [loadTailwindPlugin()],
 }
